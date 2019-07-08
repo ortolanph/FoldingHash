1480ba3e77d3ed512b441177c21c89fe054c495c
Paulo Ortolan <paulo.ortolan@gmail.com>
2019-07-08 08:20:49 +0000

    Adding group iterable

---
+++ src/main/java/pho/hash/core/folding/GroupList.java
@@ -0,0 +1,13 @@
+package pho.hash.core.folding:
+
+import java.util.Iterable;
+
+public class GroupList implements Iterable<Integer> {
+
+    public GroupList(String)
+
+    @Override
+    public Iterator<Integer> iterartor() {
+        return null;
+    }
+}
