5e45539e664a3c6b8d94030f8aab0473fe12f65f
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2019-07-10 22:00:04 +0200

    Foldinghash tests

--- src/test/java/pho/hash/core/folding/FoldingHashTest.java
+++ src/test/java/pho/hash/core/folding/FoldingHashTest.java
@@ -57,5 +57,93 @@ class FoldingHashTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void sample4GrouSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
 
+        int expected = 13334;
+        int actual = hasher.calculateHash(SAMPLE_4);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample5GrouSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
+
+        int expected = 63536;
+        int actual = hasher.calculateHash(SAMPLE_5);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample6GrouSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
+
+        int expected = 43637;
+        int actual = hasher.calculateHash(SAMPLE_6);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample6GrouSize2() {
+        Hasher hasher = new FoldingHash(GroupSize.TWO);
+
+        int expected = 20304;
+        int actual = hasher.calculateHash(SAMPLE_6);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample7GroupSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
+
+        int expected = 20375;
+        int actual = hasher.calculateHash(SAMPLE_7);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample7GroupSize3() {
+        Hasher hasher = new FoldingHash(GroupSize.THREE);
+
+        int expected = 43708;
+        int actual = hasher.calculateHash(SAMPLE_7);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample8GroupSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
+
+        int expected = 27476;
+        int actual = hasher.calculateHash(SAMPLE_8);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample9GroupSizeAuto() {
+        Hasher hasher = new FoldingHash(GroupSize.AUTO);
+
+        int expected = 60910;
+        int actual = hasher.calculateHash(SAMPLE_9);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sample9GroupSize2() {
+        Hasher hasher = new FoldingHash(GroupSize.TWO);
+
+        int expected = 27549;
+        int actual = hasher.calculateHash(SAMPLE_9);
+
+        assertEquals(expected, actual);
+    }
 }
